c61b23ca067e5e7efa430ffca9aa5899f1528f30
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2023-08-22 17:12:17 -0700

    declared the dprintf in our header file

--- monty.h
+++ monty.h
@@ -1,9 +1,16 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+
+int dprintf(int fd, const char *format, ...);
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
